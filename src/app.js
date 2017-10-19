ee47a55c8607fc47013599b7d3933710211e1986
williamsduarte <williamduarteoficial@gmail.com>
2017-10-19 17:25:48 -0200

    final mod 01

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
 'use strict'
 
-import React from "react";
+import React from 'react'
 
-var Title = React.createClass({
-    render: function() {
-        return <h1>Titulo 2</h1>
-    }
+const App = React.createClass({
+  render: function () {
+    return <h1>Aplicação 100000</h1>
+  }
 })
 
-export default Title
+export default App
